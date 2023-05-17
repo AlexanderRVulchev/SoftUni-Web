fc4cd0d8369c301d8ed95afd03bfd19e43e37ae9
AlexanderRVulchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2023-05-17 17:55:23 +0300

    ChatApp

---
+++ Workshops and Exercises/02. ChatApp/Controllers/ChatController.cs
@@ -0,0 +1,38 @@
+﻿using ChatApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ChatApp.Controllers
+{
+    public class ChatController : Controller
+    {
+        private static List<KeyValuePair<string, string>> Messages = new();
+
+        public IActionResult Show()
+        {
+            if (!Messages.Any())
+            {
+                return View(new ChatViewModel());
+            }
+
+            var chatModel = new ChatViewModel()
+            {
+                Messages = Messages
+                    .Select(m => new MessageViewModel
+                    {
+                        Sender = m.Key,
+                        MessageText = m.Value
+                    })
+                    .ToList()
+            };
+
+            return View(chatModel);
+        }
+
+        [HttpPost]
+        public IActionResult Send(ChatViewModel chat)
+        {
+            Messages.Add(new(chat.CurrentMessage.Sender, chat.CurrentMessage.MessageText));
+            return RedirectToAction(nameof(Show));
+        }
+    }
+}
