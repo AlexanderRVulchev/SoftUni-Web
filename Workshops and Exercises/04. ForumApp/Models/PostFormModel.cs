dc53a52184ff29782815f5da0b37d17cb3c698e5
AlexanderRVulchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2023-05-19 18:08:07 +0300

    ForumApp

---
+++ Workshops and Exercises/04. ForumApp/Models/PostFormModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ForumApp.Models
+{
+    public class PostFormModel
+    {
+        [Required]
+        [StringLength(50, MinimumLength = 10)]
+        public string Title { get; set; } = null!;
+
+        [Required]
+        [StringLength(1500, MinimumLength = 30)]
+        public string Content { get; set; } = null!;
+    }
+}
