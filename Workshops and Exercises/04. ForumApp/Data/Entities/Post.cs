a2e142f416d35dcfb5b1b22bebd9c24e1ccab145
AlexanderRVulchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2023-05-19 18:07:50 +0300

    ForumApp

---
+++ Workshops and Exercises/04. ForumApp/Data/Entities/Post.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ForumApp.Data.Entities
+{
+    using static ForumApp.Data.DataConstants.Post;
+
+    public class Post
+    {
+        public int Id { get; init; }
+
+        [Required]
+        [MaxLength(TitleMaxLength)]
+        public string Title { get; set; } = null!;
+
+        [MaxLength(ContentMaxLength)]
+        [Required]
+        public string Content { get; set; } = null!;
+    }
+}
