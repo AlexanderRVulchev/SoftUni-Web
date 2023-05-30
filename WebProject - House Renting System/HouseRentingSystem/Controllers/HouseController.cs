570d525ede15877410fad31a29a404eafa179c44
AlexanderRVulchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2023-05-30 18:45:18 +0300

    HouseRenting: Added navigation links and created the House controller, model and view

---
+++ WebProject - House Renting System/HouseRentingSystem/Controllers/HouseController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HouseRentingSystem.Controllers
+{
+    using Core.Models.House;
+
+    public class HouseController : Controller
+    {
+        public IActionResult All()
+        {
+            var model = new AllHousesQueryModel();
+
+            return View(model);
+        }
+    }
+}
--- WebProject - House Renting System/HouseRentingSystem/Program.cs
+++ WebProject - House Renting System/HouseRentingSystem/Program.cs
@@ -9,7 +9,15 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(connectionString));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
-builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+builder.Services.AddDefaultIdentity<IdentityUser>(options => 
+{
+    options.SignIn.RequireConfirmedAccount = false;
+    options.Password.RequireDigit = false;
+    options.Password.RequireNonAlphanumeric = false;
+    options.Password.RequireLowercase = false;
+    options.Password.RequireUppercase = false;
+    options.Password.RequiredLength = 4;
+})
     .AddEntityFrameworkStores<ApplicationDbContext>();
 builder.Services.AddControllersWithViews();
 
