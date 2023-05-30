b0656847a06bde65104b82d1afa20f0d16515745
AlexanderRVulchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2023-05-30 20:32:42 +0300

    HouseRenting: Implemented repository pattern to Core and added service collection extension.

---
+++ WebProject - House Renting System/HouseRentingSystem/Extensions/HouseRentingServiceCollectionExtension.cs
@@ -0,0 +1,14 @@
+﻿using HouseRentingSystem.Infrastructure.Data.Common;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    public static class HouseRentingServiceCollectionExtension
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+        {
+            services.AddScoped<IRepository, Repository>();
+
+            return services;
+        }
+    }
+}
